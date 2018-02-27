467c76dd85b834415ff944d700dd9f1c1fb62ee6
Nikola Radanović <33860854+radanovicnikola93@users.noreply.github.com>
2018-02-27 19:08:08 +0100

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -8,25 +8,16 @@ print("- If the number is divisible with 5 it will display buzz")
 print("- if both it will display fizzbuzz")
 print ''
 
-while True:
 
-    numbers = raw_input("Please enter a number from 1 to 100:")
-    numbers = int(numbers)
+numbers = raw_input("Please enter a number from 1 to 100:")
+numbers = int(numbers)
 
-    for choice in range(1, numbers+1):
-        if choice % 3 == 0 and choice % 5 == 0:
-            print "fizzbuzz"
-        elif choice % 3 == 0:
-            print "fizz"
-        elif choice % 5 == 0:
-            print "buzz"
-        else:
-            print choice
-
-    print ""
-    repeat = raw_input("Repeat game? Y/N")
-    if repeat.upper() == "Y":
-        print numbers
+for choice in range(1, numbers+1):
+    if choice % 3 == 0 and choice % 5 == 0:
+        print "fizzbuzz"
+    elif choice % 3 == 0:
+        print "fizz"
+    elif choice % 5 == 0:
+        print "buzz"
     else:
-        print("Thank you and goodbye!")
-        break
+        print choice
